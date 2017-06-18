c9b39e9381048059b49a0df7965056ca42683190
dani <dani.zaghian@gmail.com>
2017-06-18 16:21:18 -0700

    ng-click

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -6,6 +6,10 @@
 
     $scope.reviews = ["AngularJS is the best thing since sliced bread", "I love AngularJS, I couldn't possibly breathe without it", "AngularJS rocks my socks off", "AngularJS syntax suxxxxxx!!"];
 
+    $scope.addReview = function(newReview){
+      $scope.reviews.push(newReview);
+    };
+
     window.$scope = $scope;
   });
 })();
